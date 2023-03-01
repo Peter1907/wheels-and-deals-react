34ae0abab21c8e919885dca8700a4d06890216e4
Peter1907 <peter.nady.1408@gmail.com>
2023-03-01 18:58:48 +0200

    Add the Home component & Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 // import './App.css';
 
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
   );
 }
 
