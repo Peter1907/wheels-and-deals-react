d20718cc5666dbfea228084b318d0a4095af9743
Peter1907 <peter.nady.1408@gmail.com>
2023-03-02 00:01:39 +0200

    Update home page styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,20 @@
-const Home = () => {
-  const background = './background.jpg';
-
-  return (
-    <div className="home">
-      <img src={background} alt="background" className="fixed top-0 left-0" />
-      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50">
-        <div className="container mx-auto h-full flex flex-col justify-center items-center">
-          <h1 className="text-5xl text-white font-bold">Wheels and Deals</h1>
-          <p className="text-2xl text-white">Explore elite luxury and class</p>
-          <button type="button" className="bg-white text-black font-bold py-2 px-8 rounded-full mt-4">
-            Explore
-          </button>
-        </div>
-      </div>
-      <div className="absolute top-0 right-20 flex gap-4">
-        <button type="button" className="bg-white text-black font-bold py-2 px-4 rounded-full mt-4">LOGIN</button>
-        <button type="button" className="bg-white text-black font-bold py-2 px-4 rounded-full mt-4">SIGN UP</button>
+const Home = () => (
+  <div className="home flex flex-col">
+    <div className="absolute bg-center bg-cover bg-home h-full w-full bg-orange bg-blend-multiply" />
+    <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center">
+      <div className="flex flex-col my-auto justify-center items-center p-4 bg-black bg-opacity-20 rounded-lg">
+        <h1 className="tracking-wider text-3xl text-white font-bold mb-4 sm:text-5xl sm:tracking-widest">WHEELS AND DEALS</h1>
+        <p className="tracking-wide font-ibm text-md text-slate-300 mb-12 sm:text-2xl">Explore elite luxury and class</p>
+        <button type="button" className="bg-green text-white font-semibold py-2 px-8 rounded-full mt-4 mt-26 sm:text-2xl">
+          Explore
+        </button>
       </div>
     </div>
-  );
-};
+    <div className="absolute top-0 self-center flex justify-between w-[95%] mt-8 text-sm font-semibold sm:text-xl">
+      <button type="button" className="bg-black bg-opacity-30 text-slate-300 py-2 px-6 rounded-full hover:bg-white hover:text-black">LOGIN</button>
+      <button type="button" className="bg-black bg-opacity-30 text-slate-300 py-2 px-6 rounded-full hover:bg-white hover:text-black">SIGN UP</button>
+    </div>
+  </div>
+);
 
 export default Home;
