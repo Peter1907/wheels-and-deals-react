71df22e0dc91df52386dad821f5ef9a6b3176787
Diego Mero <diegomero_1999@hotmail.com>
2023-03-03 11:07:20 -0500

    Add Navigation component

--- src/components/NewCar/NewCar.js
+++ src/components/NewCar/NewCar.js
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const logo = './logo.svg';
 
 function NewCar(onSubmit) {
   const [carData, setCarData] = useState({
@@ -23,7 +26,10 @@ function NewCar(onSubmit) {
 
   return (
     <section className="flex flex-col items-center justify-center bg-[url('/src/assets/new-car-background.jpg')] bg-center bg-cover h-screen w-screen bg-orange bg-blend-multiply">
-      <div className="bg-gray-200 rounded-full px-4 py-2 mt-2 focus:outline-none focus:bg-white absolute top-3 left-3">User Diego</div>
+      <Link to="/cars" className="w-16 z-10 self-center mt-4 sm:w-20 absolute top-1">
+        <img src={logo} alt="logo" />
+      </Link>
+      <div className="bg-gray-200 rounded-full px-4 py-2 mt-2 focus:outline-none focus:bg-white fixed top-3 left-3">User Diego</div>
       <h1 className="text-4xl font-bold text-white">ADD A NEW CAR</h1>
       <span className="block h-0.5 w-1/2 bg-white m-2.5" />
       <form onSubmit={handleSubmit} className="flex flex-col gap-2">
@@ -31,7 +37,7 @@ function NewCar(onSubmit) {
         <input className="bg-gray-200 rounded-full px-4 py-2 mt-2 focus:outline-none focus:bg-white" placeholder="Description" type="text" name="description" id="description" value={carData.description} onChange={handleInputChange} />
         <input className="bg-gray-200 rounded-full px-4 py-2 mt-2 focus:outline-none focus:bg-white" placeholder="Photo" type="text" name="photo" id="photo" value={carData.photo} onChange={handleInputChange} />
         <input className="bg-gray-200 rounded-full px-4 py-2 mt-2 focus:outline-none focus:bg-white" placeholder="Price" type="number" step="1" name="price" id="price" value={carData.price} onChange={handleInputChange} />
-        <button type="submit" className="bg-green text-black font-bold py-2 px-4 rounded-full mt-4">Add now</button>
+        <button type="submit" className="bg-green text-white font-semibold py-2 px-8 rounded-full mt-4 mt-26 sm:text-2xl">Add now</button>
       </form>
     </section>
   );
--- src/components/pages/Cars.js
+++ src/components/pages/Cars.js
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
 import Car from '../Car';
+import Navigation from '../Navigation';
 
 const Cars = () => {
   const data = [
@@ -36,18 +36,8 @@ const Cars = () => {
   const width = window.innerWidth;
   return (
     <div className="cars-page">
-      <div className="navigation hidden border-r border-gray-300 float-left py-8 w-[250px] bg-slate-100 h-screen lg:block">
-        <Link to="/">
-          <img className="w-24 mx-auto" src="./logo.svg" alt="logo" />
-        </Link>
-        <ul className="flex flex-col mt-16 ml-4">
-          <li className="cursor-pointer py-4 text-2xl font-semibold pl-8 bg-green text-white">MODELS</li>
-          <li className="cursor-pointer py-4 text-2xl font-semibold pl-8">LIFESTYLE</li>
-          <li className="cursor-pointer py-4 text-2xl font-semibold pl-8">SHOP</li>
-          <li className="cursor-pointer py-4 text-2xl font-semibold pl-8">TEST DRIVE</li>
-        </ul>
-      </div>
-      <div className="main-section flex flex-col relative justify-center">
+      <Navigation />
+      <div className="lg:ml-[250px] main-section flex flex-col relative justify-center">
         <h1 className="text-3xl text-gray-800 tracking-wider font-bold text-center mt-6 md:text-5xl md:tracking-widest md:mt-12">LATEST MODELS</h1>
         <h4 className="text-sm text-gray-700 font-ibm font-light text-center mt-2 md:text-xl md:tracking-widest md:mt-4">We have the best cars in the world</h4>
         <hr className="w-24 self-center m-4 border-t-black md:w-52 md:m-8" />
