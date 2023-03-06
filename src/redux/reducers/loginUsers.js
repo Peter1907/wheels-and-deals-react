6f064fcd6e130364c4f291875202a7c7f488109c
IngDavidTM <david-1011@hotmail.com>
2023-03-06 16:46:06 -0500

    Create reducers

---
+++ src/redux/reducers/loginUsers.js
@@ -0,0 +1,50 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const API_URL = 'http://localhost:3000/api/login';
+const POST_LOGIN = 'wheels_and_deals/login/POST_LOGIN';
+
+const initailState = {
+  token: null,
+  signed: !!sessionStorage.getItem('token'),
+  message: null,
+};
+
+const loginReducer = (state = initailState, action) => {
+  switch (action.type) {
+    case `${POST_LOGIN}/fulfilled`:
+      sessionStorage.setItem('token', action.payload.token);
+      return {
+        ...state,
+        token: action.payload.token,
+        signed: true,
+        message: 'Login Successful',
+      };
+    case `${POST_LOGIN}/rejected`:
+      return {
+        ...state,
+        token: null,
+        signed: false,
+        message: 'Login Failed',
+      };
+    default:
+      return state;
+  }
+};
+
+export const postLogin = createAsyncThunk(POST_LOGIN, async (user) => {
+  const response = await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  });
+  if (!response.ok) {
+    const error = await response.json();
+    throw new Error(error.message || 'Unable to create user');
+  }
+
+  return response.json();
+});
+
+export default loginReducer;
