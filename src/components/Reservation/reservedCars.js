ba84a56182e546f28b4f8e217516b026a499d681
Cindy Dorantes <cindym.dorantes@gmail.com>
2023-03-06 11:13:08 -0600

    Add Navigation component to ReservedCars

--- src/components/Reservation/reservedCars.js
+++ src/components/Reservation/reservedCars.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import Navigation from '../Navigation';
 import Car from './Car';
 
 const ReservedCars = () => {
@@ -43,20 +43,8 @@ const ReservedCars = () => {
 
   return (
     <div className="reserved-cars">
-      <div className="navigation hidden border-r border-gray-300 float-left py-8 w-[250px] bg-slate-100 h-screen lg:block">
-        <Link to="/">
-          <img className="w-24 mx-auto" src="./logo.svg" alt="logo" />
-        </Link>
-        <ul className="flex flex-col mt-16 ml-4">
-          <Link to="/cars">
-            <li className="cursor-pointer py-4 text-2xl font-semibold pl-8">MODELS</li>
-          </Link>
-          <li className="cursor-pointer py-4 text-2xl font-semibold pl-8">LIFESTYLE</li>
-          <li className="cursor-pointer py-4 text-2xl font-semibold pl-8">SHOP</li>
-          <li className="cursor-pointer py-4 text-2xl font-semibold pl-8 bg-green text-white">RESERVED</li>
-        </ul>
-      </div>
-      <div className="main-section flex flex-col relative justify-center">
+      <Navigation />
+      <div className="lg:ml-[250px] main-section flex flex-col relative justify-center">
         <h1 className="text-3xl text-gray-800 tracking-wider font-bold text-center mt-6 md:text-5xl md:tracking-widest md:mt-12">RESERVED MODELS</h1>
         <h4 className="text-sm text-gray-700 font-ibm font-light text-center mt-2 md:text-xl md:tracking-widest md:mt-4">Here are the models you have reserved</h4>
         <hr className="w-24 self-center m-4 border-t-black md:w-52 md:m-8" />
