e236dc9a9ac0e8325cc3736eafe4f0d01dd2a9eb
Diego Mero <diegomero_1999@hotmail.com>
2023-03-06 23:31:46 -0500

    Add car reducer to configStore

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import usersReducer from './reducers/createUsers';
 import loginReducer from './reducers/loginUsers';
 import reservationReducer from './reducers/reservation';
+import addCarReducer from './reducers/addCar';
 
 const store = configureStore({
   reducer: {
     usersCreate: usersReducer,
     loginUsers: loginReducer,
     reservations: reservationReducer,
+    newCar: addCarReducer,
   },
 });
 
